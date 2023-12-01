2a3e825c340c514011a8d93b1768e12835f0a22b
Iryna7ca <gorpinichvorona@gmail.com>
2023-12-01 20:22:27 +0100

    task-01

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,29 @@
+const buttonStart = document.querySelector("[data-start]");
+const buttonStop = document.querySelector("[data-stop]")
+const body = document.querySelector("body");
+ 
+ 
+const onClick = () => {
+    intervalId = setInterval(() => {
+        const color = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`
+        body.style.backgroundColor = color;
+        buttonStart.setAttribute('disabled', '');
+        buttonStop.removeAttribute('disabled', '');
+    }, 1000);
+};
+ 
+
+const offClick = () => {
+    clearInterval(intervalId);
+    buttonStart.removeAttribute('disabled', '');
+    buttonStop.setAttribute('disabled', '');
+};
+
+buttonStart.addEventListener("click", onClick);
+buttonStop.addEventListener("click", offClick);
+
+
+ 
+  
+ 
+ 
