55efead190545aaf5e93e30f8e7e62876aecdc33
Iryna7ca <gorpinichvorona@gmail.com>
2023-12-01 21:42:19 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,14 +3,17 @@ const buttonStop = document.querySelector("[data-stop]")
 const body = document.querySelector("body");
  
  
-const onClick = () => {
-    intervalId = setInterval(() => {
-        const color = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`
-        body.style.backgroundColor = color;
-        buttonStart.setAttribute('disabled', '');
-        buttonStop.removeAttribute('disabled', '');
-    }, 1000);
-};
+// const onClick = () => {
+//     intervalId = setInterval(() => {
+//         const color = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`
+//         body.style.backgroundColor = color;
+//         buttonStart.setAttribute('disabled', '');
+//         buttonStop.removeAttribute('disabled', '');
+//     }, 1000);
+          
+    
+// };
+ 
  
 
 const offClick = () => {
@@ -19,7 +22,14 @@ const offClick = () => {
     buttonStop.setAttribute('disabled', '');
 };
 
-buttonStart.addEventListener("click", onClick);
+buttonStart.addEventListener("click", () => {
+    intervalId = setInterval(() => {
+        const color = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`
+        body.style.backgroundColor = color;
+        buttonStart.setAttribute('disabled', '');
+        buttonStop.removeAttribute('disabled', '');
+    }, 1000);
+});
 buttonStop.addEventListener("click", offClick);
 
 
