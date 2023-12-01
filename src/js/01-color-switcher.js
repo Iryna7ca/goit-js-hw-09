4f5c7313ff1ef5a7a8e1db56ae6aba60a79cb074
Iryna7ca <gorpinichvorona@gmail.com>
2023-12-01 22:13:55 +0100

    update5

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,7 +2,7 @@ const buttonStart = document.querySelector("[data-start]");
 const buttonStop = document.querySelector("[data-stop]")
 const body = document.querySelector("body");
 
-let intervalId = null;
+let intervalId;
 
  const onClick = () => {
     intervalId = setInterval(() => {
