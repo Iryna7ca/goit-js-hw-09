0411ff28f69f8a9400c71e3e2afbcd1ec9ab883f
Iryna7ca <139339616+Iryna7ca@users.noreply.github.com>
2023-12-01 21:08:36 +0000

    Deploying to gh-pages from @ Iryna7ca/goit-js-hw-09@61fd20cc346918ad76f7cbb5e1afd387d257c1e5 🚀

--- 01-color-switcher.d6f6300d.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){intervalId=setInterval((function(){var a="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));r.style.backgroundColor=a,t.setAttribute("disabled",""),e.removeAttribute("disabled","")}),1e3)})),e.addEventListener("click",(function(){clearInterval(intervalId),t.removeAttribute("disabled",""),e.setAttribute("disabled","")}))}();
-//# sourceMappingURL=01-color-switcher.d6f6300d.js.map
